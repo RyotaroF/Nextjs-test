236f74a29bec2b353c93963af25e1e6d6e03d984
Ryotaro Furu <ryotrzs@gmail.com>
2019-08-26 19:43:41 +0900

    next test

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/MyLayout';
+
+const aboutPgeContent = <p>About Page</p>;
+
+const About = () => {
+    return (
+        <Layout content={aboutPgeContent} />
+    )
+};
+
+export default About;
